db8fd68212dd212e17d9497d4fc2cfddf5999afd
MariaCarol <86033398+EstudanteMariaCarol@users.noreply.github.com>
2022-04-28 16:51:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var { usuario } = require("./models");
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.get("/", async function(req,res){
+  const resul = await usuario.findAll(req.body);
+  res.json(resul);
+});
+
+app.post("/",function(req,res){
+  var resultado = usuario.create(req.body);
+  res.json(resultado);
+});
+
+app.listen(3000,function(){
+  console.log("ihulll")
+});
