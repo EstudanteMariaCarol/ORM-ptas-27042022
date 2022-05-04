7962185e2f272f45cfc85f5a73f8fbdd6513841b
MariaCarol <86033398+EstudanteMariaCarol@users.noreply.github.com>
2022-05-04 21:01:26 +0000

    fiz o atualizar e deletar orm

--- index.js
+++ index.js
@@ -7,15 +7,25 @@ app.use(express.urlencoded({
   extended: true
 }))
 
-app.get("/", async function(req,res){
-  const resul = await usuario.findAll(req.body);
-  res.json(resul);
-});
+app.get("/", async function(req, res){
+  var mostra = await usuario.findAll();
+  res.json(mostra)
+})
 
 app.post("/",function(req,res){
   var resultado = usuario.create(req.body);
   res.json(resultado);
 });
+
+app.put("/:id", function(req, res){
+  var atualizar = usuario.update(req.body, {where: {id: req.params.id} });
+  res.json(atualizar)
+})
+
+app.delete("/:id", function(req, res){
+  var deletar = usuario.destroy({where: {id: req.params.id} });
+  res.json(deletar)
+})
 
 app.listen(3000,function(){
   console.log("ihull")
