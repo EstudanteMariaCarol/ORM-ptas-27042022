3cc663e28d24387db65448d773af854967d4e990
MariaCarol <86033398+EstudanteMariaCarol@users.noreply.github.com>
2022-05-18 20:52:59 +0000

    adicionamos uma rota para procurar usuários pela empresa e empresa por usuários, também tivemos um contra tempo por causa da repl

--- index.js
+++ index.js
@@ -1,55 +1,86 @@
 var express = require("express");
 var app = express();
-var { usuario } = require("./models");
-var { empresa } = require("./models");
+var { usuario } = require("./models")
+var { empresa } = require("./models")
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }))
 
-app.get("/usuarios", async function(req, res){
-  var mostra = await usuario.findAll();
-  res.json(mostra)
+app.get("/usuarios/:id/empresa", async function (req, res){
+  const resultado = await usuario.findByPk(req.params.id, {include: 'empresa'});
+  res.json(resultado.empresa);
 })
 
-app.post("/usuarios",function(req,res){
-  var resultado = usuario.create(req.body);
+app.get("/usuarios/:id", async function (req, res){
+ const resultado =  await usuario.findOne({where : {id:req.params.id}});
+  res.json(resultado)
+  
+});
+
+app.get("/usuarios", async function (req, res){
+ const resultado =  await usuario.findAll();
+  res.json(resultado)
+  
+});
+
+app.post("/usuarios", function (req, res){
+ var resultado = usuario.create(req.body);
   res.json(resultado);
 });
 
-app.put("/usuarios/:id", function(req, res){
-  var atualizar = usuario.update(req.body, {where: {id: req.params.id} });
-  res.json(atualizar)
+app.get("/empresa/:id/usuarios", async function(req,res){
+  let resultado = await usuario.findByPk(req.params.id, {
+    include: 'empresa'
+  });
+  res.json(resultado.empresa);
 })
 
-app.delete("/usuarios/:id", function(req, res){
-  var deletar = usuario.destroy({where: {id: req.params.id} });
-  res.json(deletar)
-})
-
-//empresas
-
-app.get("/empresas", async function(req, res){
-  var show = await empresa.findAll();
-  res.json(show)
+app.put("/usuarios/:id", function (req, res){
+ var resultado =  usuario.update(req.body, {where : {id:req.params.id}});
+ res.json(resultado) 
 });
 
-app.post("/empresas",function(req,res){
-  var resultado = empresa.create(req.body);
+app.delete("/usuarios/:id", async function (req, res){
+ var resultado =  await usuario.destroy({where : {id:req.params.id}});
+ res.json(resultado) 
+});
+
+
+/*Empresas*/
+
+
+app.get("/empresas/:id", async function (req, res){
+ const resultado =  await empresa.findOne({where : {id:req.params.id}});
+  res.json(resultado)
+  
+});
+
+app.get("/empresas", async function (req, res){
+ const resultado =  await empresa.findAll();
+  res.json(resultado)
+  
+});
+
+app.post("/empresas", function (req, res){
+ var resultado = empresa.create(req.body);
   res.json(resultado);
 });
 
-app.put("/empresas/:id", function(req, res){
-  var atualizando = empresa.update(req.body, {where: {id: req.params.id} });
-  res.json(atualizando)
-})
+app.put("/empresas/:id", function (req, res){
+ var resultado =  empresa.update(req.body, {where : {id:req.params.id}});
+ res.json(resultado) 
+});
 
-app.delete("/empresas/:id", function(req, res){
-  var deletando = empresa.destroy({where: {id: req.params.id} });
-  res.json(deletando)
-})
+app.delete("/empresas/:id", async function (req, res){
+ var resultado =  await empresa.destroy({where : {id:req.params.id}});
+ res.json(resultado) 
+});
 
-app.listen(3001, function(){
+
+
+app.listen(3000, function(){
   console.log("ihull")
 });
+
